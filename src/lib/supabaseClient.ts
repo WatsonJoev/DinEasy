bdfea29f25d7586599b6de6497b35d1625b0f019
Sriman G <147331234+Sriman484@users.noreply.github.com>
2025-08-16 17:07:31 +0530

    Update supabaseClient.ts
    transferred to env files

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kdxafujzyofzrqndfuhg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkeGFmdWp6eW9menJxbmRmdWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzI4NTEsImV4cCI6MjA2NTkwODg1MX0.q-yDQtXxWzvQyUuf0GIxqug6X0-BCOV8qM8ECfD3kos'
+const supabaseUrl = 
+const supabaseAnonKey = 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
